55f2eabdbabd0fe90a5701ca534ff9ee42aaea72
mirqwa <kimrodrikwa@gmail.com>
2025-06-07 10:37:07 +0300

    import libraries

---
+++ main.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.cross_decomposition import PLSRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
