d906023a92bba556f7630dfea18416ebdf738de4
mirqwa <kimrodrikwa@gmail.com>
2025-06-07 10:43:30 +0300

    train and evaluate pls model

--- main.py
+++ main.py
@@ -16,5 +16,18 @@ def get_data():
     return X_train, X_test, y_train, y_test
 
 
+def train_and_evaluate_pls_model(X_train, X_test, y_train, y_test):
+    n_components = 3
+    pls_model = PLSRegression(n_components=n_components)
+    pls_model.fit(X_train, y_train)
+    y_pred = pls_model.predict(X_test)
+    # Evaluate the model performance
+    r_squared = pls_model.score(X_test, y_test)
+    print(f"R-Squared Error: {r_squared}")
+    mse = mean_squared_error(y_test, y_pred)
+    print(f"Mean Squared Error: {mse}")
+
+
 if __name__ == "__main__":
     X_train, X_test, y_train, y_test = get_data()
+    train_and_evaluate_pls_model(X_train, X_test, y_train, y_test)
