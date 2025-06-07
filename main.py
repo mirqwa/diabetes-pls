b741e9798b5c6c39e74487baa4c6debe89c3e7da
mirqwa <kimrodrikwa@gmail.com>
2025-06-07 10:40:09 +0300

    split train and test data

--- main.py
+++ main.py
@@ -6,5 +6,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
 
-if __name__ == "__main__":
+def get_data():
     diabetes = datasets.load_diabetes()
+    X = diabetes.data
+    y = diabetes.target
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+    return X_train, X_test, y_train, y_test
+
+
+if __name__ == "__main__":
+    X_train, X_test, y_train, y_test = get_data()
